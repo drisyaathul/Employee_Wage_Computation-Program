1ba4699b88870a96b679a46dcddd20eee7fba2b4
drisyaathul <drisyak809@gmail.com>
2023-03-04 15:33:44 +0800

    Added: Employee Wages till a condition of Total Working Hours or Days

--- src/com/bridgelabz/EmployeeWageComputation.java
+++ src/com/bridgelabz/EmployeeWageComputation.java
@@ -3,33 +3,48 @@ package com.bridgelabz;
 import java.util.Random;
 
 public class EmployeeWageComputation {
+    static final int empWagePerHour = 20;
+    static final int maxWorkingHours = 100;
+    static final int maxWorkingDays = 20;
     public static void main(String[] args) {
         System.out.println("** Welcome to Employee Wage Computation Program **");
 
         Random random = new Random();
-        int attendance = random.nextInt(3);
-        int empWagePerHour = 20;
+
         int salaryPerDay;
         int empHour = 0;
         int workingDays = 20;
-        int empMonthlyWage;
+        int totalWorkingHours = 0;
+        int totalWage = 0;
 
-        System.out.println("Attendance = " + attendance);
+        for (int day = 1; day <= workingDays; day++) {
+            int attendance = random.nextInt(3);
+            //System.out.println("Attendance = " + attendance);
+            System.out.println();
 
-        switch (attendance) {
-            case 0:
-                empHour = 0;
-                break;
-            case 1:
-                empHour = 8;
-                break;
-            case 2:
-                empHour = 4;
-                break;
+            switch (attendance) {
+                case 0:
+                    empHour = 0;
+                    System.out.println("Employee is absent");
+                    break;
+                case 1:
+                    empHour = 8;
+                    System.out.println("Employee is Working Full-Day");
+                    break;
+                case 2:
+                    empHour = 4;
+                    System.out.println("Employee is Working Half-Day");
+                    break;
+            }
+            salaryPerDay = empHour * empWagePerHour;
+            totalWorkingHours += maxWorkingHours;
+            totalWage += salaryPerDay;
+            System.out.println("Day:"+day);
+            System.out.println("Work Hours:"+empHour);
+            System.out.println("Daily Wage = "+salaryPerDay);
         }
-        salaryPerDay = empHour * empWagePerHour;
-        System.out.println("Employee Salary per Day = " + salaryPerDay);
-        empMonthlyWage = salaryPerDay * workingDays;
-        System.out.println("Employee Monthly Wages = " + empMonthlyWage);
+        System.out.println("---------------------------------------------");
+        System.out.println("Employee Total Wages for a month = "+totalWage);
+
     }
 }
