31ec577c0a0958581e924e7b6fad609238135bbf
drisyaathul <drisyak809@gmail.com>
2023-03-04 14:44:41 +0800

    Added: Employee Wage per Day

--- src/com/bridgelabz/EmployeeWageComputation.java
+++ src/com/bridgelabz/EmployeeWageComputation.java
@@ -5,12 +5,19 @@ import java.util.Random;
 public class EmployeeWageComputation {
     public static void main(String[] args) {
         System.out.println("** Welcome to Employee Wage Computation Program **");
+
         Random random = new Random();
         int attendance = random.nextInt(2);
-        if (attendance == 1) {
-            System.out.println("Employee is Present");
-        } else {
-            System.out.println("Employee is Absent");
+        int empWagePerHour = 20;
+        int FullDayHour = 8;
+        int salaryPerDay;
+        System.out.println("Attendance = "+attendance);
+
+        salaryPerDay = attendance*FullDayHour*empWagePerHour;
+        if(attendance == 1){
+            System.out.println("Employee Daily Wage = "+salaryPerDay);
+        }else {
+            System.out.println("Employee Daily Wage = "+salaryPerDay);
         }
     }
 }
